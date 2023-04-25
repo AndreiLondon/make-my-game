fe4565a59c523505128e226cfe5c2c67db069ff6
AndrewLondon <andrewmart@mail.com>
2023-04-25 23:13:12 +0100

    pacman

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-const canvas = document.getElementById("canvas"); 
+//Pac-Man game developing with Andrei London
+
+const canvas = document.getElementById("canvas");
 const canvasCtx = canvas.getContext("2d");
 
 const pacmanFrames = document.getElementById("animation");
@@ -8,6 +10,10 @@ let createRect = (x, y, width, height, color) => {
     canvasCtx.fillStyle = color;
     canvasCtx.fillRect(x, y, width, height);
 };
+
+let oneBlockSize = 20;
+const fps = 60; // Target frame rate
+let wallColor = "#342DCA";
 
 // we now create the map of the walls,
 // if 1 wall, if 0 not wall
@@ -37,3 +43,37 @@ let map = [
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+
+let gameLoop = () => {
+    update();
+    draw();
+};
+
+let update = () => {
+
+};
+
+let draw = () => {
+
+};
+
+// let gameInterval = setInterval(gameLoop, 1000/fps)
+ 
+
+const intervalDuration = 1000 / fps; // Interval duration in milliseconds
+
+let gameInterval = setInterval(gameLoop, intervalDuration);
+
+let drawWalls = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 1) { // then it is a wall
+                createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor);
+            }
+        }
+    }
+}; 
+
+
+
